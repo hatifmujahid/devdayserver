ff7efdba8554b5f1e14d8b08ec3633282e228d6f
hatifmujahid <hatifmujahid08@gmail.com>
2024-03-13 00:53:52 +0500

    sad

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const app = express();
-app.use(cors());
+app.use(cors({origin: 'https://acmdevday.com'}));
 app.use(bodyParser.json());
 
 const connectionString = process.env.MONGOURI || '';
@@ -25,47 +25,17 @@ const db = client.db(); // Get the default database
 
 // Define schema and model for Participants
 const participantCollection = db.collection('participants');
-// const participantSchema = {
-//     Team_Name: { type: 'string', required: true },
-//     Leader_name: { type: 'string', required: true },
-//     Leader_email: { type: 'string', required: true },
-//     Leader_whatsapp_number: { type: 'string', required: true },
-//     mem1_name: 'string',
-//     mem1_email: 'string',
-//     mem1_whatsapp_number: 'string',
-//     mem2_name: 'string',
-//     mem2_email: 'string',
-//     mem2_whatsapp_number: 'string',
-//     fees_amount: { type: 'number', required: true },
-//     paid: { type: 'boolean', default: false },
-//     reference_code: { type: 'string', required: true }
-// };
+
 
 // Define schema and model for FYP_Registration
 const fypRegistrationCollection = db.collection('fyp_registrations');
-// const fypRegistrationSchema = {
-//     Team_Name: { type: 'string', required: true },
-//     Leader_name: { type: 'string', required: true },
-//     Leader_email: { type: 'string', required: true },
-//     Leader_whatsapp_number: { type: 'string', required: true },
-//     mem1_name: 'string',
-//     mem1_email: 'string',
-//     mem1_whatsapp_number: 'string',
-//     mem2_name: 'string',
-//     mem2_email: 'string',
-//     mem2_whatsapp_number: 'string',
-//     category: { type: 'string', required: true },
-//     fees_amount: { type: 'decimal128', required: true },
-//     paid: { type: 'boolean', required: true, default: false },
-//     reference_code: { type: 'string', required: true }
-// };
 
 // Routes
 app.post('/devdaynodeapi/addParticipant', async (req, res) => {
     try {
         const participant = req.body;
         const result = await participantCollection.insertOne(participant);
-        res.send(result.ops[0]);
+        res.send('Participant added successfully');
     } catch (error) {
         console.error(error);
         res.status(500).send('Error saving participant');
