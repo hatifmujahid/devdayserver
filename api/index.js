c711605fc5631ec4403fb227dcd94cbb7f62ea4d
SameerVers3 <sameerghafoor34@gmail.com>
2024-03-29 18:37:50 +0500

    forgot one thingy

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ const { stringify } = require('querystring');
 const app = express();
 const port = 5000;
 
+app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(bodyParser.json());
