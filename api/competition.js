d2b84a21141186117357f089a2e9cb58b07d29b8
Muhammad Hatif Mujahid <56687118+hatifmujahid@users.noreply.github.com>
2024-04-10 19:01:20 +0500

    Update competition.js

--- api/competition.js
+++ api/competition.js
@@ -19,6 +19,11 @@ const getCompetitionDetails = (competition) => {
             return roboticsCompetitions[i][competition];
         }
     }
+    for (let i = 0; i < esports.length; i++) {
+        if (esports[i].hasOwnProperty(competition)) {
+            return esports[i][competition];
+        }
+    }
 
     return null;
 }
@@ -262,6 +267,12 @@ const getCompetitionID = (competition) => {
     for (let i = 0; i < roboticsCompetitions.length; i++) {
         if (roboticsCompetitions[i].name === competition) {
             id = roboticsCompetitions[i].id;
+        }
+    }
+
+    for (let i = 0; i < esports.length; i++) {
+        if (esports[i].name === competition) {
+            id = esports[i].id;
         }
     }
 
