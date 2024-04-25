ddbb15f78828c93d74742b6f5a5b1c8231faf0db
SameerVers3 <sameerghafoor34@gmail.com>
2024-04-25 13:38:35 +0500

    done

--- api/index.js
+++ api/index.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-app.use(cors(corsOptions)) // Use this after the variable declaration
+const cors = require('cors');
 const bodyParser = require('body-parser');;
 const ftp = require('basic-ftp');
 const dotenv = require('dotenv');
